3b1a8d33af78af728c3918c16dcec18d91bd8c2f
Damien Jeannerat <damien.jeannerat@unige.ch>
2020-04-05 00:36:05 +0200

    second

--- runme.js
+++ runme.js
@@ -1,67 +1,68 @@
 // https://www.c-sharpcorner.com/article/creating-server-and-host-html-page-using-node-js/
-    var http = require('http');  
-    var url = require('url');  
-    var fs = require('fs');  
-    var server = http.createServer(function(request, response) {  
-        var path = url.parse(request.url).pathname;  
-        switch (path) {  
-            case '/':  
-                response.writeHead(200, {  
-                    'Content-Type': 'text/plain'  
-                });  
-                response.write("This is Test Message !!.");  
-                response.end();  
-                break;  
-            case '/html/HtmlPage1.html':  
-                fs.readFile(__dirname + path, function(error, data) {  
-                    if (error) {  
-                        response.writeHead(404);  
-                        response.write(error);  
-                        response.end();  
-                    } else {  
-                        response.writeHead(200, {  
-                            'Content-Type': 'text/html'  
-                        });  
-                        response.write(data);  
-                        response.end();  
-                    }  
-                });  
-                break;  
-                case '/html/HtmlPage2.html':  
-                fs.readFile(__dirname + path, function(error, data) {  
-                    if (error) {  
-                        response.writeHead(404);  
-                        response.write(error);  
-                        response.end();  
-                    } else {  
-                        response.writeHead(200, {  
-                            'Content-Type': 'text/html'  
-                        });  
-                        response.write(data);  
-                        response.end();  
-                    }  
-                });  
-                break;  
-            case '/index.html':  
-                fs.readFile(__dirname + path, function(error, data) {  
-                    if (error) {  
-                        response.writeHead(404);  
-                        response.write(error);  
-                        response.end();  
-                    } else {  
-                        response.writeHead(200, {  
-                            'Content-Type': 'text/html'  
-                        });  
-                        response.write(data);  
-                        response.end();  
-                    }  
-                });  
-                break;  
-            default:  
-                response.writeHead(404);  
-                response.write("opps this doesn't exist - 404");  
-                response.end();  
-                break;  
-        }  
-    });  
-    server.listen(5500);  
+import { readFile } from 'fs';
+import { createServer } from 'http';
+import { parse } from 'url';
+
+let server = createServer(function (request, response) {
+  let path = parse(request.url).pathname;
+  switch (path) {
+    case '/':
+      response.writeHead(200, {
+        'Content-Type': 'text/plain',
+      });
+      response.write('This is Test Message !!.');
+      response.end();
+      break;
+    case '/html/HtmlPage1.html':
+      readFile(__dirname + path, function (error, data) {
+        if (error) {
+          response.writeHead(404);
+          response.write(error);
+          response.end();
+        } else {
+          response.writeHead(200, {
+            'Content-Type': 'text/html',
+          });
+          response.write(data);
+          response.end();
+        }
+      });
+      break;
+    case '/html/HtmlPage2.html':
+      readFile(__dirname + path, function (error, data) {
+        if (error) {
+          response.writeHead(404);
+          response.write(error);
+          response.end();
+        } else {
+          response.writeHead(200, {
+            'Content-Type': 'text/html',
+          });
+          response.write(data);
+          response.end();
+        }
+      });
+      break;
+    case '/index.html':
+      readFile(__dirname + path, function (error, data) {
+        if (error) {
+          response.writeHead(404);
+          response.write(error);
+          response.end();
+        } else {
+          response.writeHead(200, {
+            'Content-Type': 'text/html',
+          });
+          response.write(data);
+          response.end();
+        }
+      });
+      break;
+    default:
+      response.writeHead(404);
+      response.write("opps this doesn't exist - 404");
+      response.end();
+      break;
+  }
+});
+server.listen(5500);
